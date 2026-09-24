347975a7e923b8d2121a49767de045327e467cef
seriiserii825 <seriiburduja@gmail.com>
2026-09-24 21:26:16 +0300

    feat: remove a few options

--- src/py_libs/Select.py
+++ src/py_libs/Select.py
@@ -27,7 +27,7 @@ class Select:
     @staticmethod
     def select_questionary(options: List[str]) -> List[str]:
         selected = questionary.checkbox(
-            "Select a few options:", choices=options).ask()
+            "Select options:", choices=options).ask()
         return selected
 
     @staticmethod
